c6e0f4450df4d1564e77c4a0272667036f3e187d
Teagan-E-Embrey <143535333+Teagan-E-Embrey@users.noreply.github.com>
2023-08-29 23:39:07 +0000

    Updated home page.

--- js/homepagescroll.js
+++ js/homepagescroll.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const sections = document.querySelectorAll(".inner-section");
+    const sectionDots = document.querySelectorAll(".section-dot");
     let currentSectionIndex = 0;
     const scrollableContainer = document.querySelector(".scrollable-section");
 
@@ -15,14 +16,33 @@ document.addEventListener("DOMContentLoaded", function () {
         // Scroll to the active section
         const activeSection = sections[index];
         scrollableContainer.scrollTop = activeSection.offsetTop;
+
+        // Update active dot
+        sectionDots.forEach((dot, i) => {
+            if (i === index) {
+                dot.classList.add("active");
+            } else {
+                dot.classList.remove("active");
+            }
+        });
     }
+
+    sectionDots.forEach((dot, index) => {
+        dot.addEventListener("click", function () {
+            showSection(index);
+            currentSectionIndex = index;
+        });
+    });
 
     function rotateSections() {
         currentSectionIndex = (currentSectionIndex + 1) % sections.length;
+        if (currentSectionIndex === 3) {
+            currentSectionIndex = 0;
+        }
         showSection(currentSectionIndex);
     }
 
-    setInterval(rotateSections, 10000); // Rotate every 10 seconds
+    setInterval(rotateSections, 5000); // Rotate every 10 seconds
 
     showSection(currentSectionIndex);
 });
